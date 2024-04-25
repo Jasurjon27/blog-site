40f3a67a66af7d599129f6c05fead0ce5a0691a4
Jasur <botirovjasurjon72@gmail.com>
2024-04-25 22:03:36 +0300

    pp

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from models import Post
+from models import Category,Tag,Author,Post
 # Create your views here.
 
 
 def home_page(request):
-    posts = Post.objects
+    posts = Post.objects.all().order_by('-id')[:6]
+    context = {'posts':posts}
+    return render(request,'index.html', context)
